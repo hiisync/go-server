90af78a3ebb3dace47c61ccba54646d2a477c786
Savchin <stiv.asakura@gmail.com>
2024-05-15 13:38:01 +0300

    moved:    internal/server/routes.go -> internal/routes/routes.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,12 +2,13 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/hiisync/werix-server/internal/routes"
 )
 
 func StartServer() {
 	r := gin.Default()
 
-	Routes(r)
+	routes.Routes(r)
 
 	r.Run()
 }
